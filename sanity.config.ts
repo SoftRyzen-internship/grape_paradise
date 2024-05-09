9e7c569e8db002d7e099fd30c23ecdfe0301beb6
Symeon Melnychuk <m.simeon998@gmail.com>
2024-05-09 13:35:57 +0300

    bag fix

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,11 @@
-import {defineConfig} from './node_modules/sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
+import { defineConfig } from './node_modules/sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+
 import { schemaTypes } from './src/sanity/schemas/index'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 
 export default defineConfig({
