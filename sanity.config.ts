5de1838ea759175b6948ad211ddf26f59a2dc6c3
Symeon Melnychuk <m.simeon998@gmail.com>
2024-05-09 12:27:48 +0300

    synchronization of sanity with the project, adding configuration files

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from './node_modules/sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import { schemaTypes } from './src/sanity/schemas/index'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
+
+
+export default defineConfig({
+  basePath: '/admin',
+  name: 'default',
+  title: 'Grape Paradise',
+
+  projectId: projectId,
+  dataset: dataset,
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
