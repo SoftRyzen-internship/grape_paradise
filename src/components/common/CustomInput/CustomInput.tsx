06336f5383d00108b1f92a1a7548dbfc7246bfec
DisRachik <radchenkoig@gmail.com>
2024-05-09 11:25:46 +0300

    create component input

---
+++ src/components/common/CustomInput/CustomInput.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { forwardRef, useId } from 'react';
+import clsx from 'clsx';
+
+// import styles from "./CustomInput.module.css"
+import { ICustomInputProps } from './CustomInput.types';
+
+export const CustomInput = forwardRef<HTMLInputElement, ICustomInputProps>(
+	({ label, errorMessage, className, ...rest }, ref) => {
+		const id = useId();
+
+		return (
+			<div className={clsx('relative', className)}>
+				{label ? <label htmlFor={id}>{label}</label> : null}
+				<input {...rest} ref={ref} id={id} />
+				{errorMessage ? (
+					<div>
+						<span>{errorMessage}</span>
+					</div>
+				) : null}
+			</div>
+		);
+	},
+);
+
+CustomInput.displayName = 'Input';
