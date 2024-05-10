d744f21e361222410219b959f5e5d798afdebf50
parasolya <o.shevchuk@gmail.com>
2024-05-10 14:41:52 +0300

    fixed css in Image ProductCard

--- src/components/ui/ProductCard.tsx/ProdactCard.tsx
+++ src/components/ui/ProductCard.tsx/ProdactCard.tsx
@@ -12,17 +12,16 @@ import { IProductCardProps } from './ProductCard.types';
 export const ProdudtCard: FC<IProductCardProps> = ({ index = 0 }) => {
 	return (
 		<>
-			<div className='bg-bg-2 mx-auto w-fill sm:w-[480px] md:w-[334px] lg:w-[521px]'>
-                
+			<div className='bg-bg-2 w-fill mx-auto sm:w-[480px] md:w-[334px] lg:w-[521px]'>
 				<h3 className='text-h3 lg:text-h3_desk'>
 					{productCardData[index].title}
 				</h3>
 
 				<div
-					className={`relative mx-auto mb-4 mt-6 h-[229px] w-fill sm:w-[480px] md:mb-6 md:h-[274px] md:w-[334px] lg:mb-10 lg:mt-10 lg:w-[521px] ${styles.containerImg} overflow-hidden`}
+					className={`w-fill relative mx-auto mb-4 mt-6 h-[229px] sm:w-[480px] md:mb-6 md:h-[274px] md:w-[334px] lg:mb-10 lg:mt-10 lg:w-[521px] ${styles.containerImg} overflow-hidden`}
 				>
 					<Image
-						className='z-5 absolute left-0 right-auto top-0 object-cover'
+						className='object-cover'
 						fill
 						src={productCardData[index].img.src}
 						alt={productCardData[index].img.alt}
@@ -33,7 +32,6 @@ export const ProdudtCard: FC<IProductCardProps> = ({ index = 0 }) => {
 				<ButtonMain chapter='card' to='контакти'>
 					{buttonsData.orderButton}
 				</ButtonMain>
-
 			</div>
 		</>
 	);
