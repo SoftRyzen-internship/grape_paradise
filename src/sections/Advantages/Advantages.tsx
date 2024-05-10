5fd802c55013a99e59158f65f1011baf9a3eba59
Andrii Gadar <gadar.andre@gmail.com>
2024-05-10 21:23:30 +0300

    added style for icons

--- src/sections/Advantages/Advantages.tsx
+++ src/sections/Advantages/Advantages.tsx
@@ -11,9 +11,9 @@ import s from './Advantages.module.css';
 export const Advantages: FC = () => (
 	<Section
 		sectionStyles={`relative py-[60px] md:py-20 lg:py-[183px] ${s.advantages_bg}`}
-		containerStyles={`${s.leaf}`}
+		containerStyles={`relative ${s.leaf}`}
 	>
-		<div className={`${s.bottle}`}>
+		<div className={`relative ${s.bottle}`}>
 			<Title styles='mb-6 w-[255px] md:w-[350px] md:mb-10 lg:w-[590px] lg:mb-16'>
 				{advantagesData.title}
 			</Title>
