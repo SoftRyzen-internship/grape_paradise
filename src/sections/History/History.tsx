f0ebfe86f4ee93f83b10aaf318d17f6a639cb956
Andrii Gadar <gadar.andre@gmail.com>
2024-05-11 13:31:12 +0300

    added id for history section

--- src/sections/History/History.tsx
+++ src/sections/History/History.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image';
 
 import { Section } from '@/components/common/Section';
 import { Title } from '@/components/common/Title';
-
-import { historyData } from '@/data';
 import { ButtonMain } from '@/components/ui/ButtonMain';
 import { ButtonLess } from '@/components/ui/ButtonLess';
+
+import { historyData } from '@/data';
 
 import Play from '@/../public/icons/play.svg';
 
 import s from './History.module.css';
 
 export const History: FC = () => (
-	<Section sectionStyles='pt-[60px] pb-[30px]'>
+	<Section id={historyData.id} sectionStyles='pt-[60px] pb-[30px]'>
 		<div className={`relative mb-12 h-[328px] ${s.frame}`}>
 			<Image
 				fill
@@ -31,18 +31,20 @@ export const History: FC = () => (
 			</div>
 		</div>
 
-		<Title styles='mb-4 w-[320px] md:w-[334px] md:mb-6 lg:w-[488px]'>
-			{historyData.title}
-		</Title>
+		<div>
+			<Title styles='mb-4 w-[320px] md:w-[334px] md:mb-6 lg:w-[488px]'>
+				{historyData.title}
+			</Title>
 
-		<p className='mb-3'>{historyData.description}</p>
+			<p className='mb-3'>{historyData.description}</p>
 
-		<p className='mb-6'>{historyData.offer}</p>
+			<p className='mb-6'>{historyData.offer}</p>
 
-		<div className='grid'>
-			<ButtonMain chapter='about' to='contacts'>
-				{historyData.button}
-			</ButtonMain>
+			<div className='grid'>
+				<ButtonMain chapter='about' to='contacts'>
+					{historyData.button}
+				</ButtonMain>
+			</div>
 		</div>
 	</Section>
 );
