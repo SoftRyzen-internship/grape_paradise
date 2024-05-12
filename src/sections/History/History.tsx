ae54bd9410385c78a77829995a4cc399e5e4776e
Andrii Gadar <gadar.andre@gmail.com>
2024-05-12 23:50:11 +0300

    added style for tablet and desktop device

--- src/sections/History/History.tsx
+++ src/sections/History/History.tsx
@@ -13,8 +13,14 @@ import Play from '@/../public/icons/play.svg';
 import s from './History.module.css';
 
 export const History: FC = () => (
-	<Section id={historyData.id} sectionStyles='pt-[60px] pb-[30px]'>
-		<div className={`relative mb-12 h-[328px] ${s.frame}`}>
+	<Section
+		id={historyData.id}
+		sectionStyles='pt-[60px] pb-[30px] md:pt-20 md:pb-10 lg:pt-[142px] lg:pb-20'
+		containerStyles={`md:flex md:items-center md:justify-between lg:relative ${s.grape}`}
+	>
+		<div
+			className={`relative mb-12 h-[328px] md:m-0 md:h-[312px] md:w-[300px] lg:h-[579px] lg:w-[567px] ${s.frame}`}
+		>
 			<Image
 				fill
 				priority
@@ -27,22 +33,24 @@ export const History: FC = () => (
 			<ButtonLess
 				type='button'
 				purpose='video'
-				styles='absolute -bottom-5 right-5'
+				styles='absolute -bottom-5 right-5 md:bottom-[42px] md:-right-8 lg:bottom-[109px] lg:-right-11 z-10'
 			>
 				{<Play className='h-[26px] w-[26px] lg:h-[64px] lg:w-[64px]' />}
 			</ButtonLess>
 		</div>
 
-		<div>
-			<Title styles='mb-4 w-[320px] md:w-[334px] md:mb-6 lg:w-[488px]'>
+		<div className='md:w-[334px] lg:w-[488px]'>
+			<Title styles='mb-4 w-[320px] md:mb-6 md:w-full'>
 				{historyData.title}
 			</Title>
 
-			<p className='mb-3'>{historyData.description}</p>
+			<p className='mb-3 md:mb-4 lg:mb-6 lg:text-normal_desk'>
+				{historyData.description}
+			</p>
 
-			<p className='mb-6'>{historyData.offer}</p>
+			<p className='lg:normal_desk mb-6 lg:mb-[52px]'>{historyData.offer}</p>
 
-			<ButtonMain chapter='about' to='contacts'>
+			<ButtonMain chapter='about' to={historyData.link}>
 				{historyData.button}
 			</ButtonMain>
 		</div>
