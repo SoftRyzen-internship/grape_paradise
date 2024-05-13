48e3435734e27776f4a4e54260a0334892d6bd1a
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2024-05-13 23:55:37 +0300

    one more conflict

--- src/data/index.ts
+++ src/data/index.ts
@@ -14,11 +14,8 @@ export { default as logo } from './logo.json';
 export { default as formData } from './formData.json';
 
 export { default as headerData } from './headerData.json';
-<<<<<<< HEAD
 export { default as heroData } from './heroData.json';
-=======
 export { default as historyData } from './historyData.json';
->>>>>>> dev
 
 export { default as movingLineData } from './movingLineData.json';
 
