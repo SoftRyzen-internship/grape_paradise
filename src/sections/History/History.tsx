69f7a2e8ecce00fac2e46e092b1d79f651fa4711
Andrii Gadar <gadar.andre@gmail.com>
2024-05-13 22:16:35 +0300

    the section is divided into components

--- src/sections/History/History.tsx
+++ src/sections/History/History.tsx
@@ -1,16 +1,10 @@
 import { FC } from 'react';
-import Image from 'next/image';
 
 import { Section } from '@/components/common/Section';
-import { Title } from '@/components/common/Title';
-import { ButtonMain } from '@/components/ui/ButtonMain';
-import { ButtonLess } from '@/components/ui/ButtonLess';
+import { HistoryFrame } from '@/components/common/HistoryFrame';
+import { HistoryContent } from '@/components/common/HistoryContent';
 
 import { historyData } from '@/data';
-
-import Play from '@/../public/icons/play.svg';
-
-import s from './History.module.css';
 
 export const History: FC = () => (
 	<Section
@@ -18,41 +12,7 @@ export const History: FC = () => (
 		sectionStyles='pt-[60px] pb-[30px] md:pt-20 md:pb-10 lg:pt-[142px] lg:pb-20'
 		containerStyles='md:flex md:items-center md:justify-between'
 	>
-		<div
-			className={`relative smOnly:mb-12 h-[328px] md:h-[312px] md:w-[300px] lg:h-[579px] lg:w-[567px] ${s.grape}`}
-		>
-			<Image
-				fill
-				priority
-				src={historyData.img}
-				alt={historyData.alt}
-				className={s.frame}
-				sizes='(max-width: 768px) 448px, (max-width: 1280px) 300px, 567px'
-			/>
-
-			<ButtonLess
-				type='button'
-				purpose='video'
-				styles='absolute -bottom-5 right-5 md:bottom-[42px] md:-right-8 lg:bottom-[109px] lg:-right-11 z-10'
-			>
-				{<Play className='h-[26px] w-[26px] lg:h-[64px] lg:w-[64px]' />}
-			</ButtonLess>
-		</div>
-
-		<div className='md:w-[334px] lg:w-[488px]'>
-			<Title styles='mb-4 w-[320px] md:mb-6 md:w-full'>
-				{historyData.title}
-			</Title>
-
-			<p className='mb-3 md:mb-4 lg:mb-6 lg:text-normal_desk'>
-				{historyData.description}
-			</p>
-
-			<p className='lg:normal_desk mb-6 lg:mb-[52px]'>{historyData.offer}</p>
-
-			<ButtonMain chapter='about' to={historyData.link}>
-				{historyData.button}
-			</ButtonMain>
-		</div>
+		<HistoryFrame />
+		<HistoryContent />
 	</Section>
 );
