22636e3ba2b59b42b134e0b993f52a1d4a4932e6
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2024-05-14 21:48:54 +0300

    create markup of footer-mob

---
+++ readme/Hero.md
@@ -0,0 +1,3 @@
+### Hero component
+
+Include background image with a torn effect, main title and text-description.
--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -4,19 +4,33 @@ import clsx from 'clsx';
 
 import { Logo } from '@/components/ui/Logo';
 
-// import Foot from '../../../public/icons/bg-footer-form-frame-top.svg';
 import Field from '@/../public/images/footer/footer.svg';
+import GoIT from '@/../public/icons/goit-icon.svg';
+import SoftRyzen from '@/../public/icons/softryzen-icon.svg';
+
+import { footerData } from '@/data';
 
 import s from './Footer.module.css';
+import Link from 'next/link';
 
 export const Footer: FC = () => {
 	return (
-		<footer className={clsx(s.footer, 'w-full bg-bg2 pb-8')}>
+		<footer className={clsx(s.footer, 'w-full bg-bg2 pb-8 pt-7')}>
 			<Field />
 			<div className='container'>
-				{/* <Foot className='fill-black' /> */}
-				<div className='mx-auto block'>
+				<div className='mb-8 flex justify-center'>
 					<Logo theme={'green'} size={'big'} />
+				</div>
+				<div className='flex flex-col border-t-2 border-stroke pt-8'>
+					<p className='mx-auto mb-4 text-center'>{footerData.create}</p>
+					<div className='mx-auto mb-8 flex w-[211px] items-baseline gap-4'>
+						<GoIT width={64} height={19} />
+						<SoftRyzen className='w-[131px] fill-green' />
+					</div>
+					<Link href={'/policy'} className='mb-8 text-center'>
+						{footerData.policy}
+					</Link>
+					<p className='text-center'>{footerData.rights}</p>
 				</div>
 			</div>
 		</footer>
