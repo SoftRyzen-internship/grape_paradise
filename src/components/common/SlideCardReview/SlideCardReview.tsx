855c3a9f8dcb5edbea6b80a15ffe1585328a99bb
Symeon Melnychuk <m.simeon998@gmail.com>
2024-05-14 12:58:49 +0300

    add type layout

--- src/components/common/SlideCardReview/SlideCardReview.tsx
+++ src/components/common/SlideCardReview/SlideCardReview.tsx
@@ -8,7 +8,7 @@ export const SlideCardReviews = () => {
 				'flex h-[331px] flex-col justify-between bg-white px-[24px] py-[36px] md:h-[331px] md:w-[334px] lg:h-[485px] lg:w-[580px] lg:px-[64px] lg:py-[56px]',
 			)}
 		>
-			<p className=' lg:text-h2'>
+			<p className='lg:text-h2'>
 				Добрий день, пані Наталя. Ще раз вам дякую за прекрасні смаколики і
 				візит
 			</p>
