f9af4a533fa674dd4bb1ee4a7904869074f4b364
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2024-05-14 03:47:40 +0300

    add bg svg-image & bg color

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -4,15 +4,17 @@ import clsx from 'clsx';
 
 import { Logo } from '@/components/ui/Logo';
 
-import Foot from '../../../public/icons/bg-footer-form-frame-top.svg';
+// import Foot from '../../../public/icons/bg-footer-form-frame-top.svg';
+import Field from '@/../public/images/footer/footer.svg';
 
 import s from './Footer.module.css';
 
 export const Footer: FC = () => {
 	return (
-		<footer className={clsx(s.footer, 'w-full pb-8 pt-28')}>
+		<footer className={clsx(s.footer, 'w-full bg-bg2 pb-8')}>
+			<Field />
 			<div className='container'>
-				<Foot className='fill-black' />
+				{/* <Foot className='fill-black' /> */}
 				<div className='mx-auto block'>
 					<Logo theme={'green'} size={'big'} />
 				</div>
