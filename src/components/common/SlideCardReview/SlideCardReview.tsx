92ec290c52d8c232bd09578983ee05f1a5377322
Symeon Melnychuk <m.simeon998@gmail.com>
2024-05-14 14:00:42 +0300

    2

--- src/components/common/SlideCardReview/SlideCardReview.tsx
+++ src/components/common/SlideCardReview/SlideCardReview.tsx
@@ -1,12 +1,13 @@
 import { clsx } from 'clsx';
-import Image from '../../../../node_modules/next/image';
+// import Image from '../../../../node_modules/next/image';
 
 export const SlideCardReviews = () => {
 	return (
 		<div
 			className={clsx(
-				'flex h-[331px] flex-col justify-between bg-white px-[24px] py-[36px] md:h-[331px] md:w-[334px] lg:h-[485px] lg:w-[580px] lg:px-[64px] lg:py-[56px]',
+				'flex h-[331px] flex-col justify-between px-[24px] py-[36px] md:h-[331px] md:w-[334px] lg:h-[485px] lg:w-[580px] lg:px-[64px] lg:py-[56px]',
 			)}
+            style="background-image: url('/public/icons/bg-reviews.svg')"
 		>
 			<p className='lg:text-h2'>
 				Добрий день, пані Наталя. Ще раз вам дякую за прекрасні смаколики і
