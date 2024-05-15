7186d6a35439770f3f328f4fdca8ef9976f97333
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-05-15 17:20:38 +0300

    Changed component to async

--- src/sections/Services/Services.tsx
+++ src/sections/Services/Services.tsx
@@ -1,31 +1,20 @@
-'use client';
-
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 
 import { Section } from '@/components/common/Section';
 import { Title } from '@/components/common/Title';
 
-import { servicesData } from '@/data';
-import { IDataCardGeneration } from '@/components/common/SlideCardGeneration/SlideCardGeneration.types';
 import { fetchServices } from '@/sanity/request/fetchServices';
 
-export const Services: FC = () => {
-	const [services, setServices] = useState<IDataCardGeneration[]>([]);
-	
-	console.log('services', services);
+import { servicesData } from '@/data';
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const servicesSanity = await fetchServices();
-			setServices(servicesSanity);
-		};
 
-		fetchData();
-	}, []);
+export const Services: FC = async () => {
+	const services = await fetchServices();
+	console.log('services', services[0].imageServices);
 
 	return (
 		<Section id={servicesData.id} sectionStyles='py-[30px] md:py-10 lg:py-20'>
-			<Title styles='smOnly:mb-6 smOnly:w-[129px]'>{servicesData.title}</Title>
+			<Title className='smOnly:mb-6 smOnly:w-[129px]'>{servicesData.title}</Title>
 		</Section>
 	);
 };
