4f168c131fadc707528da9e272f530e9bacbe604
DisRachik <radchenkoig@gmail.com>
2024-05-21 19:57:51 +0300

    add smooth scroll for logo into footer

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { FC } from 'react';
 import Link from 'next/link';
+import { Link as LinkScroll } from 'react-scroll';
 
 import { LogoSvg } from './LogoSvg';
 
@@ -7,18 +10,39 @@ import { logo } from '@/data';
 
 import { ILogoProps } from './Logo.types';
 
-export const Logo: FC<ILogoProps> = ({ theme = 'white', size = 'small' }) => {
+export const Logo: FC<ILogoProps> = ({
+  theme = 'white',
+  size = 'small',
+  toScroll,
+}) => {
   return (
     <>
-      <Link
-        className='cursor-pointer focus:outline-1 focus:outline-white'
-        href='/'
-        aria-label={logo.ariaLabel}
-      >
-        <div className='inline-block'>
-          <LogoSvg theme={theme} size={size} />
-        </div>
-      </Link>
+      {toScroll ? (
+        <LinkScroll
+          href='#'
+          to='hero'
+          smooth={true}
+          spy={true}
+          duration={500}
+          offset={0}
+          className='cursor-pointer focus:outline-1 focus:outline-white'
+          aria-label={logo.ariaLabel}
+        >
+          <div className='inline-block'>
+            <LogoSvg theme={theme} size={size} />
+          </div>
+        </LinkScroll>
+      ) : (
+        <Link
+          href='/'
+          className='cursor-pointer focus:outline-1 focus:outline-white'
+          aria-label={logo.ariaLabel}
+        >
+          <div className='inline-block'>
+            <LogoSvg theme={theme} size={size} />
+          </div>
+        </Link>
+      )}
     </>
   );
 };
